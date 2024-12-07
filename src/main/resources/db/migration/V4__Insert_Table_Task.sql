748b67b00ca368d1a315301bfe0eb88b2408f923
Lucas Peixoto <lspeixotodev@gmail.com>
2024-12-06 21:29:37 -0300

    Update env file config

--- src/main/resources/db/migration/V4__Insert_Table_Task.sql
+++ src/main/resources/db/migration/V4__Insert_Table_Task.sql
@@ -1,2 +1,2 @@
 INSERT INTO task (id, title, completed, category_id, created_at, updated_at) VALUES
-	('01b23c6f-0c7c-4530-bf62-3cef6f9ed9d3', 'Treinar', false, 'e72596db-9734-4468-97c2-66a1f039eff3', '2024-11-26 13:31:30.000000', '2023-11-26 22:31:30.000000');
+	('01b23c6f-0c7c-4530-bf62-3cef6f9ed9d3', 'Começar a marcar minhas tarefas', false, 'b4c8d28e-a537-4a89-86de-171da4c56c5f', '2024-12-06 13:31:30.000000', '2024-12-06 13:31:30.000000');
